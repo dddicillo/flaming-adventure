283468ab9f509e0d1ce3fa5bde437e3b02f60868
joshosu999 <trick.8@osu.edu>
2013-11-02 21:36:20 -0400

    Start2

--- FlamingAdventure/grails-app/domain/flaming/adventure/Person.groovy
+++ FlamingAdventure/grails-app/domain/flaming/adventure/Person.groovy
@@ -2,6 +2,35 @@ package flaming.adventure
 
 class Person {
 
-    static constraints = {
-    }
+
+	private int posX;
+	private int posY;
+	private String name;
+	private char status = 'x';
+
+	Person(int x, int y, String name) {
+		this.posX = x;
+		this.posY = y;
+		this.name = name;
+	}
+
+	void moveUp() {
+		posY += 1;
+	}
+
+	void moveDown() {
+		posY -= 1;
+	}
+
+	void moveRight() {
+		posX += 1;
+	}
+
+	void moveLeft() {
+		posX -= 1;
+	}
+
+
+
+
 }
