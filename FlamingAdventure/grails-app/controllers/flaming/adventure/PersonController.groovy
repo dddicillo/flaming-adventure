a5913114853fdf3c1f97251e4b8f79a513a7d8cc
joshosu999 <trick.8@osu.edu>
2013-11-03 04:04:50 -0500

    Movement made

--- FlamingAdventure/grails-app/controllers/flaming/adventure/PersonController.groovy
+++ FlamingAdventure/grails-app/controllers/flaming/adventure/PersonController.groovy
@@ -10,23 +10,68 @@ class PersonController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def title() {
-        def hero = new Person(0, 0, "Josh")
+    def world() {
+        Person hero = Person.first()
         List<List> list = new ArrayList<List>();
-        int c = 0
         for (int i = 0; i < 5; i ++) {
             def column = []
             for (int k = 0; k < 5; k++) {
-                column.add(0)
+                if (hero.posX == k && hero.posY == i) {
+                    column.add(' ')
+                }
+                else {
+                    column.add('*')
+                }
             }
             list[i] = column
         }
-        [name: hero.name, list: list]
+        def count = Person.count()
+        [posX: hero.posX, posY: hero.posY, list: list, count: count, name: hero.getName()]
     }
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Person.list(params), model:[personInstanceCount: Person.count()]
+    def moveDown() {
+        // this is stupid
+        Person hero = Person.first()
+        hero.moveDown()
+        hero.save(flush: true)
+        
+        redirect controller: 'person', action: 'world'
+    }
+
+    def moveUp() {
+        Person hero = Person.first()
+        hero.moveUp()
+        hero.save(flush: true)
+
+        redirect controller: 'person', action: 'world'
+    }
+
+    def moveRight() {
+        Person hero = Person.first()
+        hero.moveRight()
+        hero.save(flush: true)
+
+        redirect controller: 'person', action: 'world'
+    }
+
+    def moveLeft() {
+        Person hero = Person.first()
+        hero.moveLeft()
+        hero.save(flush: true)
+
+        redirect controller: 'person', action: 'world'
+    }
+
+    def error() {
+        [name: "Error"]
+    }
+
+    def index() {
+        def hero = new Person(posX: 1, posY: 1, name: "Derek")
+        if (!hero.save(flush: true, failOnError: true)) {
+            redirect controller: 'person', action: 'error'
+        }
+        redirect controller: 'person', action: 'world'
     }
 
     def show(Person personInstance) {
