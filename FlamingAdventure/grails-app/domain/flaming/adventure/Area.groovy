3257a75bf9c3e7ad078fe7fd35db0bef47beff4e
Derek DiCillo <dicillo.5@osu.edu>
2013-11-02 22:35:21 -0400

    Added Area controller

--- FlamingAdventure/grails-app/domain/flaming/adventure/Area.groovy
+++ FlamingAdventure/grails-app/domain/flaming/adventure/Area.groovy
@@ -5,28 +5,23 @@ class Area {
     static constraints = {
     }
 
-    private final int GRID_ROWS = 5;
-    private final int GRID_COLUMNS = 5;
+    private final int GRID_ROWS = 4;
+    private final int GRID_COLUMNS = 4;
+    enum terrainType {
+    	WATER(0),
+    	GRASS(1)
+    }
 
-    private List<List<Integer>> areaGrid = new ArrayList<>();
+    private List<List> areaGrid = new ArrayList();
 
-    def Area() {
+    Area() {
+    	new File("../../../lib/terrain.txt")
            for (int i = 0; i < GRID_ROWS; i++) {
-               List columns = new ArrayList<Integer>();
+               List columns = new ArrayList();
                for (int j = 0; j < GRID_COLUMNS; j++) {
-                   columns.add(0);
+                   	columns.add(terrainFile.readline());
                }
                areaGrid.add(columns);
            }
        }
 }
-
-def test = new Area();
-println test.areaGrid.size() 
-
-i = 0
-test.areaGrid.each { row ->
-        i++
-        println "Row $i\n"
-}
-
